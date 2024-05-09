b29ade24e0fdeb7cd6b98363fb2786f981620873
ErrazIG <frederic-vml@hotmail.fr>
2024-05-09 20:45:03 +0200

    relations sequelize

--- models/index.js
+++ models/index.js
@@ -1,5 +1,9 @@
 'use strict';
 import { Sequelize } from "sequelize";
+import MemberBuilder from "../models/member.model.js";
+import RoleBuilder from "../models/role.model.js";
+import SongsBuilder from "../models/song.model.js";
+import UserLikesBuilder from "../models/user-likes.model.js";
 
 
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DIALECT } = process.env;
@@ -13,5 +17,35 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 const db = {};
 
 db.sequelize = sequelize;
+
+db.Members = MemberBuilder(sequelize);
+db.Roles = RoleBuilder(sequelize);
+db.Songs = SongsBuilder(sequelize);
+db.UserLikes = UserLikesBuilder(sequelize);
+
+db.Roles.hasMany(db.Members, {
+    foreignKey: 'role_id'
+})
+db.Members.belongsTo(db.Roles, {
+    foreignKey: 'role_id'
+});
+
+db.Members.hasMany(db.Songs, {
+    foreignKey: 'member_id'
+})
+db.Songs.belongsTo(db.Members, {
+    foreignKey: 'member_id'
+});
+
+db.Members.belongsToMany(db.Songs, {
+    through: db.UserLikes,
+    foreignKey: 'member_id',
+    otherKey: 'song_id'
+});
+db.Songs.belongsToMany(db.Members, {
+    through: db.UserLikes,
+    foreignKey: 'song_id',
+    otherKey: 'member_id'
+});
 
 export default db;
