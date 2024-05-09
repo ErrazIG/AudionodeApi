55eff023d814bf5779926a263b39d1897ff3167a
ErrazIG <frederic-vml@hotmail.fr>
2024-05-09 20:13:33 +0200

    member model

---
+++ models/member.model.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import { Sequelize, DataTypes } from "sequelize";
+
+/** member model
+ * @param {Sequelize} sequelize
+ * @returns
+ */
+
+export default (sequelize) => {
+
+    const Member = sequelize.define('member', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            
+        },
+        role_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        img: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            defaultValue: "../../public/users/default",
+        },
+    }, {
+        timestamps: false,
+    });
+    return Member;
+}
